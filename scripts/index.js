6b28bb23fa93bf3963fea049a1ef9a3287b2bf2d
kitros <kitcotros2@gmail.com>
2025-09-11 21:12:06 -0500

    add cards array and corrections

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,29 @@
-//Edit Profile Functionality
+initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
 
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileBtn = document.querySelector(".profile__head-edit");
@@ -13,8 +38,6 @@ editProfileClose.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
 
-//New Post Functionality
-
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostBtn = document.querySelector(".profile__new-post-button");
 
@@ -26,4 +49,8 @@ const newPostClose = newPostModal.querySelector(".modal__close-btn");
 
 newPostClose.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
+});
+
+initialCards.forEach(function (card) {
+  console.log(card.name);
 });
